3b1034dfb6f12101197a876ba153d307feaba392
8Dion8 <shvartserinfo@gmail.com>
2023-04-01 19:12:30 +0300

    upd

--- get_file.py
+++ get_file.py
@@ -11,26 +11,29 @@ import telebot as tb
 TOKEN = os.environ.get('TOKEN')
 bot = tb.TeleBot(TOKEN)
 
-
 opts = FirefoxOptions()
-opts.add_argument("--headless")
+#opts.add_argument("--headless")
 bot.send_message(741069625, "Loading driver")
 driver = webdriver.Firefox(executable_path='geckodriver', options=opts)
 bot.send_message(741069625, "Installing addon")
 driver.install_addon("./adblock_plus-3.16.2.xpi")
-sleep(5)
 bot.send_message(741069625, "Loading page")
 driver.get("https://ttsfree.com/text-to-speech")
-
-elem = driver.find_element("xpath", "//div[@class='qc-cmp-cleanslate css-cgzk6p']")
-driver.execute_script("arguments[0].style.visibility='hidden'", elem)
+sleep(2)
+try:
+    elem = driver.find_element("xpath", "//div[@class='qc-cmp-cleanslate css-cgzk6p']")
+    driver.execute_script("arguments[0].style.visibility='hidden'", elem)
+except:pass
 
 
 bot.send_message(741069625, "Bot online.")
 
+audio_link = ""
+
 
 @bot.message_handler(func=lambda msg: True)
 def main_react(message):
+    global audio_link
     word = message.text
 
     select_click = driver.find_element("xpath","/html/body/section[2]/div[2]/form/div[2]/div[1]/div[1]/div[2]/span[1]")
@@ -51,10 +54,18 @@ def main_react(message):
     driver.execute_script("arguments[0].scrollIntoView();", start_button)
 
     start_button.click()
-    sleep(10)
     driver.execute_script("arguments[0].scrollIntoView();", start_button)
-    audio_link = driver.find_element("xpath","/html/body/section[2]/div[2]/form/div[2]/div[2]/div[3]/div[2]/audio/source[1]").get_attribute("src")
-    print(audio_link)
+    while True:
+        try:
+            link = driver.find_element("xpath","/html/body/section[2]/div[2]/form/div[2]/div[2]/div[3]/div[2]/audio/source[1]").get_attribute("src")
+            if link != audio_link:
+                audio_link = link
+            else:
+                continue
+            break
+        except:sleep(1)
+
+    #print(audio_link)
 
     r = requests.get(audio_link)
 
