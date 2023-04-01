bb53f0640b83c0b87169b787e865b163079753af
8Dion8 <shvartserinfo@gmail.com>
2023-04-01 18:02:39 +0300

    upd

---
+++ get_file.py
@@ -0,0 +1,56 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+import os
+import requests
+from time import sleep
+import telebot as tb
+
+
+
+driver = webdriver.Firefox(executable_path='geckodriver')
+driver.get("https://ttsfree.com/text-to-speech")
+
+
+TOKEN = os.environ.get('TOKEN')
+
+bot = tb.TeleBot(TOKEN)
+
+bot.send_message(741069625, "Bot online.")
+
+
+@bot.message_handler(func=lambda msg: True)
+def main_react(message):
+    word = message.text
+
+    select_click = driver.find_element_by_xpath("/html/body/section[2]/div[2]/form/div[2]/div[1]/div[1]/div[2]/span[1]")
+    driver.execute_script("arguments[0].scrollIntoView();", select_click)
+    select_click.click()
+    input_lang  = driver.find_element_by_xpath("/html/body/span/span/span[1]/input")
+    input_lang.send_keys("hebrew")
+    select_lang = driver.find_element_by_id("select2-select_lang_bin-results")
+    driver.execute_script("arguments[0].scrollIntoView();", select_lang)
+    select_lang.click()
+
+    word_input = driver.find_element_by_id("input_text")
+    driver.execute_script("arguments[0].scrollIntoView();", word_input)
+    word_input.clear()
+    word_input.send_keys(word)
+
+    start_button = driver.find_element_by_xpath("/html/body/section[2]/div[2]/form/div[2]/div[2]/a")
+    driver.execute_script("arguments[0].scrollIntoView();", start_button)
+
+    start_button.click()
+    sleep(10)
+    driver.execute_script("arguments[0].scrollIntoView();", start_button)
+    audio_link = driver.find_element_by_xpath("/html/body/section[2]/div[2]/form/div[2]/div[2]/div[3]/div[2]/audio/source[1]").get_attribute("src")
+    print(audio_link)
+
+    r = requests.get(audio_link)
+
+    with open(f"{word}.mp3", "wb") as f:
+        f.write(r.content)
+
+    
+    bot.send_audio(message.chat.id, open(f"{word}.mp3", "rb"))
+
+bot.infinity_polling()
